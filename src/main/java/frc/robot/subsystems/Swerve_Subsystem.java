71d98397d242139ecb77f0ee49671cb09295ad81
Hunter Rothman <rothmanhunterd@gmail.com>
2023-10-21 10:34:12 -0400

    Setup Swerve, Needs Test

--- src/main/java/frc/robot/subsystems/Swerve_Subsystem.java
+++ src/main/java/frc/robot/subsystems/Swerve_Subsystem.java
@@ -2,9 +2,23 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Swerve_Subsystem extends SubsystemBase {
+import java.io.File;
+import java.io.IOException;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Filesystem;
+import swervelib.parser.SwerveParser;
+import swervelib.SwerveDrive;
 
-  public Swerve_Subsystem() {}
+public class Swerve_Subsystem extends SubsystemBase {
+  File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
+  SwerveDrive Swerve = new SwerveParser(swerveJsonDirectory).createSwerveDrive();
+
+  public Swerve_Subsystem() throws IOException {
+  }
+
+  public void Drive_Swerve(Translation2d Translation, double Rotation) {
+    Swerve.drive(Translation, Rotation, true, false);
+  }
 
   @Override
   public void periodic() {
